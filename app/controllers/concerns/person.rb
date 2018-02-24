9f231199de9d1ab571167eb7b25978d1c879731a
Tiger Fu <tigerfu@berkeley.edu>
2018-02-23 18:55:04 -0800

    fuck github pt 2

---
+++ app/controllers/concerns/person.rb
@@ -0,0 +1,22 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def nickname
+    # YOUR IMPLEMENTATION HERE
+    @nickname = name[0..3]
+  end
+
+  def birth_year
+    # YOUR IMPLEMENTATION HERE
+    2018 - @age.to_i
+  end
+
+  def introduction
+    # YOUR IMPLEMENTATION HERE
+    "name: " + @name + ", age: " + @age  end
+end
